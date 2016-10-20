73cf79d7f3ee6e0f6b6efe82448ab36a1f401ee1
koundinya <maheshsharmakoundinyagavicherla@koundinya.local>
2016-10-20 07:21:20 -0500

    mysterious anchor scroll issue

--- src/home/home.controller.js
+++ src/home/home.controller.js
@@ -1,10 +1,30 @@
-function HomeController($scope,$location,$anchorScroll){
+function HomeController($scope,$location,$anchorScroll,scrollService,$timeout){
   $scope.image = "src/svg/home-img.jpg";
 $scope.buttons = [ { name: 'Home', value: 'home'},{ name:'Family Tree', value:'family-tree'},{ name:'Gallery', value: 'gallery'},{name:'Contact Us', value:'contactus'}];
- $scope.gotoSection = function(id) {
-      console.log('go to section',id);
-       $location.hash("family-tree");
-       $anchorScroll();
+ $scope.gotoSection = function(x) {
+      // console.log('go to section',x);
+      // $location.hash();
+      // $anchorScroll();
+
+
+      // var newHash = x;
+      // console.log('newHash',newHash);
+      // console.log($location.hash());
+      // if ($location.hash() !== newHash) {
+      //   // set the $location.hash to `newHash` and
+      //   // $anchorScroll will automatically scroll to it
+      //   $location.hash(x);
+      // } else {
+      //   // call $anchorScroll() explicitly,
+      //   // since $location.hash hasn't changed
+      //   $anchorScroll();
+      // }
+      console.log('go to section',x);
+      $location.hash(x);
+      $anchorScroll();
+      // scrollService.scrollTo(x);
+
+
  };
 
  // $scope.gotoNextSection = function(){
